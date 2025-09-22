27dc5705e8b4b6c51a99331a9cde570a54a441e5
Layla <layfish99@gmail.com>
2025-09-22 16:18:00 -0600

    Finished Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int percent = int.Parse(percentage);
+
+        if (percent >= 90)
+        {
+            Console.WriteLine("Your letter grade is A");
+        }
+        else if (percent >= 80)
+        {
+            Console.WriteLine("Your letter grade is B");
+        }
+        else if (percent >= 70)
+        {
+            Console.WriteLine("Your letter grade is C");
+        }
+        else if (percent >= 60)
+        {
+            Console.WriteLine("Your letter grade is D");
+        }
+        else
+        {
+            Console.WriteLine("Your letter grade is F");
+        }
     }
 }
